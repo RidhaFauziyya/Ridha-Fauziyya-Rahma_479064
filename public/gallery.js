2fea5c42a86faa77391f99582564463569692696
RidhaFauziyya <ridhafauziyyar123@gmail.com>
2022-12-01 17:38:00 +0700

    make it better

--- public/gallery.js
+++ public/gallery.js
@@ -10,6 +10,7 @@ $(document).ready(function (){
             response['galleries'].forEach(element =>{
                 gal += '<div> <a class="example-image-link" href="http://portofolioppw2.herokuapp.com/storage/project/' + element['imagePath'] + '" data-lightbox="roadtrip" data-title=""><img class="example-image img-fluid mb-2" src="http://portofolioppw2.herokuapp.com/storage/project/' + element['imagePath'] + '"alt="image-1"/></a></div>';
             });
+            
 
             $('#gallery-api').html(gal);
             console.log(gal)
